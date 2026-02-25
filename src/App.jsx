925e08726377cdf8416906c2013c8228a8ad277d
mksalinsub-prog <mksalinsub@tip.edu.ph>
2026-02-25 23:36:23 +0800

    Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { CryptoProvider } from "./context/CryptoContext";
+
 function App() {
-  return <h1>Crypto Pulse</h1>;
+  return (
+    <CryptoProvider>
+      <Router>
+        <nav style={{ padding: "20px", background: "#111", color: "white" }}>
+          <Link to="/" style={{ marginRight: "20px", color: "white" }}>
+            Market
+          </Link>
+          <Link to="/analysis" style={{ color: "white" }}>
+            Analysis
+          </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/analysis" element={<h1>Analysis Page</h1>} />
+        </Routes>
+      </Router>
+    </CryptoProvider>
+  );
 }
 
 export default App;

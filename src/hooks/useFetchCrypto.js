593770efae2735d6e6ecae82b506dce5c0a2f2b3
mksalinsub-prog <mksalinsub@tip.edu.ph>
2026-02-25 23:53:50 +0800

    Fetching Crypto

---
+++ src/hooks/useFetchCrypto.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { useCrypto } from '../context/CryptoContext';
+
+export const useFetchCrypto = () => {
+  const { setCoins, currency } = useCrypto();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMarket = async () => {
+      try {
+        setLoading(true);
+
+        const res = await fetch(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=1`
+        );
+
+        if (!res.ok) throw new Error("API Error");
+
+        const data = await res.json();
+        setCoins(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setTimeout(() => setLoading(false), 500); // show loading at least 0.5s
+      }
+    };
+
+    fetchMarket();
+  }, [currency, setCoins]);
+
+  return { loading, error };
+};
